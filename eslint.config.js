79dcc28fff8a4c1e42cda02a3e63b4435ef12bec
crepusculumx <crepusculumx@qq.com>
2024-07-01 16:49:37 +0800

    build: Add prettier eslint-plugin-prettier eslint-config-prettier.
    pnpm add -D prettier eslint-plugin-prettier eslint-config-prettier

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,9 @@
 const eslint = require("@eslint/js");
 const tseslint = require("typescript-eslint");
 const angular = require("angular-eslint");
-  
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+
+
 module.exports = tseslint.config(
   {
     files: ["**/*.ts"],
@@ -11,6 +13,7 @@ module.exports = tseslint.config(
       ...tseslint.configs.recommended,
       ...tseslint.configs.stylistic,
       ...angular.configs.tsRecommended,
+      eslintPluginPrettierRecommended,
     ],
     processor: angular.processInlineTemplates,
     rules: {
